8610bffcbde40856061a97d15a27a9ce0ee3e82d
sadiki-dev <sadiki.dev@gmail.com>
2021-04-07 13:45:13 +0100

    V1.2 Worked => add Pagination with @RequestParam

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # activite-pratique-2
 
+###Some CMD Command to fix "8080 port already taken" issue
+
+1- Tap "netstat -ao |find /i "listening" to Check what processes are running at available ports.
+
+2- Tap "Taskkill /F /IM #Process_Id (eg:2564)" To stop in windows
+
+3 - Rerun application
+
--- src/main/java/com/example/demo/web/PatientController.java
+++ src/main/java/com/example/demo/web/PatientController.java
@@ -3,9 +3,12 @@ package com.example.demo.web;
 import com.example.demo.dao.PatientRepository;
 import com.example.demo.entities.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,9 +23,11 @@ public class PatientController {
     }
 
     @GetMapping(path = "/patients")
-    public String listPatients(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients",patients);
+    public String listPatients(Model model,
+           @RequestParam(name="page",defaultValue = "0") int page ,
+           @RequestParam(name = "size",defaultValue = "5") int size){
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("patients",pagePatients.getContent());
         return "patients";
     }
 
