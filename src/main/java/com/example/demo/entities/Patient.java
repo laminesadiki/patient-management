f9345565f326c48f0530f987742facc2d81b4d58
sadiki-dev <sadiki.dev@gmail.com>
2021-04-09 19:19:30 +0100

    V2 Word => add formPatient and handle it

--- src/main/java/com/example/demo/entities/Patient.java
+++ src/main/java/com/example/demo/entities/Patient.java
@@ -1,11 +1,15 @@
 package com.example.demo.entities;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -15,9 +19,11 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//    @Column(name = "NOM",length = 25)
+    @NotNull
+    @Size(min = 5 ,max = 15,message = "Nom incorrect")
     private String nom;
-//    @Temporal(TemporalType.DATE)
+    @Temporal(TemporalType.DATE) // C'est de JPA
+    @DateTimeFormat(pattern = "yyyy-MM-dd") // C'est de Spring
     private Date dateNaissance;
 //    private int score;
     private boolean malade;
--- src/main/java/com/example/demo/web/PatientController.java
+++ src/main/java/com/example/demo/web/PatientController.java
@@ -7,9 +7,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -54,6 +55,20 @@ public class PatientController {
         patientRepository.deleteById(id);
         return listPatients(model,page,size,keyword);
     }
+
+    @GetMapping(path = "/formPatient")
+    public String formPatient(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatient";
+    }
+
+    @PostMapping(path = "/savePatient")
+    public String savePatient(@Valid @ModelAttribute("patient") Patient  patient, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) return "formPatient";
+        patientRepository.save(patient);
+        return "formPatient";
+    }
+
 
 
 
